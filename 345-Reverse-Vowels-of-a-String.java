2080e6144c17b43e39b7abec1160868a8686de6d
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2024-10-07 12:55:59 -0700

    [345] [Time Beats: 98.80%] [Memory Beats: 37.30%] - LeetPush

--- 345-Reverse-Vowels-of-a-String.java
+++ 345-Reverse-Vowels-of-a-String.java
@@ -1,32 +1,31 @@
 class Solution {
-    boolean isVowel(char c) {
+      boolean isVowel(char c) {
         return c == 'a' || c == 'i' || c == 'e' || c == 'o' || c == 'u'
                 || c == 'A' || c == 'I' || c == 'E' || c == 'O' || c == 'U';
     }
 
-    void swap(char[] chars, int start, int end) {
-        char temp = chars[start];
-        chars[start] = chars[end];
-        chars[end] = temp;
+    void swap(char[] sArr, int start, int end) {
+        char temp = sArr[start];
+        sArr[start] = sArr[end];
+        sArr[end] = temp;
     }
 
     public String reverseVowels(String s) {
-        int start = 0;
-        int end = s.length() - 1;
-        char[] sChar = s.toCharArray();
+        int start = 0, end = s.length() - 1;
+        char[] sArr = s.toCharArray();
 
         while(start < end) {
-            while(start < s.length() && !isVowel(sChar[start])) {
+            while(start < s.length() && !isVowel(sArr[start])) {
                 start++;
             }
-            while(end >= 0 && !isVowel(sChar[end])) {
+            while(end >= 0 && !isVowel(sArr[end])) {
                 end--;
             }
 
             if(start < end) {
-                swap(sChar, start++, end--);
+                swap(sArr, start++, end--);
             }
         }
-        return new String(sChar);
+        return new String(sArr);
     }
 }
