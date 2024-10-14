9165cfcc95c28c852096763e42c27e903bd7b850
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2024-10-14 12:35:44 -0700

    [605] [Time Beats: 96.24%] [Memory Beats: 47.70%] - LeetPush

--- 605-Can-Place-Flowers.java
+++ 605-Can-Place-Flowers.java
@@ -4,16 +4,18 @@ class Solution {
         for(int i = 0; i < flowerbed.length; i++) {
             int flower = flowerbed[i];
             if(flower == 0) {
-                boolean isEmptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
-                boolean isEmptyRight = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+                boolean isEmptyLeft = (i == 0) || flowerbed[i - 1] == 0;
+                boolean isEmptyRight = (i == flowerbed.length - 1) || flowerbed[i + 1] == 0;
 
                 if(isEmptyLeft && isEmptyRight) {
-                    count++;
-                    flowerbed[i] = 1;
+                count++;
+                flowerbed[i] = 1;
                 }
-
-                if(count >= n) return true;
             }
+
+
+
+            if(count >= n) return true;
         }
         return count >= n;
     }
