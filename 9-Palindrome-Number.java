7a17542dd5f873bcfa143906657cba5909bcdcf7
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2025-02-16 11:24:59 -0800

    [9] [Time Beats: 72.72%] [Memory Beats: 60.54%] - LeetPush

--- 9-Palindrome-Number.java
+++ 9-Palindrome-Number.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String intStr = String.valueOf(x);
-        int length = intStr.length();
-        for(int i = 0; i < length/2; i++) {
-            if(intStr.charAt(i) != intStr.charAt(length - 1 - i)) {
+        String intArr = Integer.toString(x);
+        int length = intArr.length();
+        for(int i = 0; i < length / 2; i++) {
+            if(intArr.charAt(i) != intArr.charAt(length - i - 1)) {
                 return false;
             }
         }
