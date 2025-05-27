34041bd6586d4f2c791569749cec3d9c7c9d8fa7
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2025-05-27 14:19:21 -0700

    [27] [Time Beats: 100.00%] [Memory Beats: 74.98%] - LeetPush

--- Remove-Element.cpp
+++ Remove-Element.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int count = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] != val) nums[count++] = nums[i];
+        for(int num : nums) {
+            if(num != val) nums[count++] = num;
         }
         return count;
     }
