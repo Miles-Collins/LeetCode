08596286c65485113220c37ceaefe101cf6edb66
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2025-06-09 21:39:24 -0700

    [268] [Time Beats: 100.00%] [Memory Beats: 38.63%] - LeetPush

--- Missing-Number.cpp
+++ Missing-Number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int num1 = 0, num2 = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            num1 = num1 ^ nums[i];
-            num2 = num2 ^ (i + 1);
-        }
-        return num1 ^ num2;
+     int XOR1 = 0, XOR2 = 0;
+     for(int i = 0; i < nums.size(); i++) {
+        XOR1 = XOR1 ^ nums[i];
+        XOR2 = XOR2 ^ (i + 1);
+     } 
+     return XOR1 ^ XOR2;  
     }
 };
