701f7ae4505f2467c7cbe70f424d8066ce86a11a
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2025-06-10 13:41:24 -0700

    [448] [Time Beats: 68.81%] [Memory Beats: 40.40%] - LeetPush

--- Find-All-Numbers-Disappeared-in-an-Array.cpp
+++ Find-All-Numbers-Disappeared-in-an-Array.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<int> arr(nums.size() + 1, 0);
-        
-        for(int i = 0; i < nums.size(); i++) {
-            arr[nums[i]] = 1; 
+        vector<int> arr(nums.size() +  1, 0);
+
+        for(int num : nums) {
+            arr[num]++;
         }
 
         vector<int> ans;
-        for(int i = 1; i <= nums.size(); i++) {
-            if(arr[i] == 0) ans.push_back(i);
+
+        for(int i = 1; i < arr.size(); i++) {
+            if(arr[i] == 0) {
+                ans.push_back(i);
+            }
         }
+
         return ans;
     }
 };
