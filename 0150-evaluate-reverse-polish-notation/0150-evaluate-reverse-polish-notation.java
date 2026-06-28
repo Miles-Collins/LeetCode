92a3ca49f79635df28eea4bbf27385e20b79f57e
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2026-06-27 22:51:14 -0700

    Time: 7 ms (35.89%), Space: 45.4 MB (45.7%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -3,7 +3,8 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
 
         for (String token : tokens) {
-            if (!token.equals("+") && !token.equals("-") && !token.equals("/") && !token.equals("*")) {
+
+            if (!token.equals("+") && !token.equals("-") && !token.equals("*") && !token.equals("/")) {
                 stack.push(Integer.parseInt(token));
             } else {
                 int b = stack.pop();
